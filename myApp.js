e60e2218ffb81b5e3b486a2d7b82338d96bd59d9
Kyle John O'Connor <kyle.oconnor@angloamerican.com>
2023-02-15 14:38:28 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res) {
     res.sendFile(absolutePath);
   });
 
-  let publicPath = __dirname + "/public";
+let publicPath = __dirname + "/public";
 app.use("/public", express.static(publicPath));
 
 
