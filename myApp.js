2b2a346e8848dd55b9fcfd5097a88961c705f0af
Kyle John O'Connor <kyle.oconnor@angloamerican.com>
2023-02-15 16:07:20 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
+
+let publicPath = __dirname + "/public";
+app.use("/public", express.static(publicPath));
+
+app.use(function(req,res,next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
 let absolutePath = __dirname + "/views/index.html"
 app.get("/", function(req, res) {
@@ -22,13 +31,7 @@ app.get("/json", function(req,res) {
     );
   });
 
-let publicPath = __dirname + "/public";
-app.use("/public", express.static(publicPath));
 
-app.use(function(req,res,next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-});
 
 
 
